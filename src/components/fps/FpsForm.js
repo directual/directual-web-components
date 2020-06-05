e8661128e107218253762f893450772f2889f074
Nikita <nikita@directual.com>
2020-06-05 21:22:58 +0300

    [FUT-099] transform fpsForm to hook style

--- src/components/fps/FpsForm.js
+++ src/components/fps/FpsForm.js
@@ -1,105 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../../styles.module.css'
-import icon from './../../icons/fps-form.svg'
 
-class FpsForm extends React.Component {
+export default function ({ data, onEvent, id }) {
+  const successText = data.successText || 'success'
+  const formName = data.formName || 'formName'
+  const error = data.error
+  const isSuccessWrite = data.isSuccessWrite
+  const fileds = data.fileds || []
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      model: {},
-      loading: false,
-      modelError: {},
-      error: props.data.error,
-      isSuccessWrite: props.data.isSuccessWrite
-    }
+  const [model, setModel] = useState({})
+  const [loading, setLoading] = useState(false)
+  const [modelError, setModelError] = useState({})
 
-    this.successText = props.data.successText || 'success'
-    this.formName = props.data.formName || 'formName'
-    this.fileds = props.data.fileds || []
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.setState({
-      error: nextProps.data.error,
-      isSuccessWrite: nextProps.data.isSuccessWrite,
-      loading: false
-    })
-    this.forceUpdate()
-  }
-
-  sendMsg = (msg) => {
-    const message = { ...msg, _id: 'form_' + this.props.id }
-    this.setState({ loading: true })
-    console.log(message)
-    if (this.props.onEvent) {
-      this.props.onEvent(message)
+  const sendMsg = (msg) => {
+    const message = { ...msg, _id: 'form_' + id }
+    setLoading(true)
+    if (onEvent) {
+      onEvent(message)
     }
   }
 
-  onChange = (field) => (e) => {
-    const modelCopy = { ...this.state.model }
+  const onChange = (field) => (e) => {
+    const modelCopy = { ...model }
     modelCopy[field] = e.target.value
-    this.setState({ model: modelCopy })
+    setModel(modelCopy)
   }
 
-  submit = (e) => {
+  const submit = (e) => {
     e.preventDefault()
+    console.log(model)
     var containsError = false
     var modelErrorCopy = {}
-    this.fileds.forEach((fields) => {
-      if (
-        !this.state.model[fields.sysName] ||
-        this.state.model[fields.sysName] === ''
-      ) {
+    fileds.forEach((fields) => {
+      if (!model[fields.sysName] || model[fields.sysName] === '') {
         modelErrorCopy[fields.sysName] = 'Please, fil the form'
         containsError = true
       }
     })
-    this.setState({ modelError: modelErrorCopy })
-    if (containsError) {
-      console.log(modelErrorCopy)
-    }
+    setModelError(modelErrorCopy)
     if (!containsError) {
-      this.sendMsg(this.state.model)
+      sendMsg(model)
     }
   }
 
-  render() {
-    return (
-      <div className={styles.test}>
-        <h1>{this.formName}</h1>
-        {this.state.loading && <div>loading...</div>}
+  return (
+    <div className={styles.test}>
+      <h1>{formName}</h1>
+      {loading && <div>loading...</div>}
 
-        {this.state.error && <div>error:{this.state.error}</div>}
-        {this.state.isSuccessWrite && <div>{this.successText}</div>}
-        {!this.state.isSuccessWrite && (
-          <form onSubmit={this.submit}>
-            {this.fileds.map((field) => (
-              <div>
-                {field.name}{' '}
-                <input name='' onChange={this.onChange(field.sysName)} />
-                {this.state.modelError[field.sysName] && (
-                  <b>{this.state.modelError[field.sysName]}</b>
-                )}
-              </div>
-            ))}
-            <input type='submit' />
-          </form>
-        )}
-      </div>
-    )
-  }
+      {error && <div>error:{error}</div>}
+      {isSuccessWrite && <div>{successText}</div>}
+      {!isSuccessWrite && (
+        <form onSubmit={submit}>
+          {fileds.map((field) => (
+            <div>
+              {field.name} <input name='' onChange={onChange(field.sysName)} />
+              {modelError[field.sysName] && <b>{modelError[field.sysName]}</b>}
+            </div>
+          ))}
+          <input type='submit' />
+        </form>
+      )}
+    </div>
+  )
 }
-
-FpsForm.settings = {
-  icon: icon,
-  name: "Form",
-  sysName: 'FpsForm',
-  form: [
-    {name: "Select SL", sysName:"sl", type:"api-enpoint"},
-    {name: "Success text", sysName:"successText", type: "input"},
-    {name: "Form name", sysName:'formName', type: "input"},
-  ]
-}
-export default FpsForm
