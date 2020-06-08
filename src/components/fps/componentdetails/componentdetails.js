62d253d11c3e8026a7a9e3611dafb6f67fd131ea
Pavel Ershov <pavel@directual.com>
2020-06-08 19:12:22 +0300

    menu

--- src/components/fps/componentdetails/componentdetails.js
+++ src/components/fps/componentdetails/componentdetails.js
@@ -3,6 +3,17 @@ import styles from './componentdetails.module.css'
 
 export default function ComponentDetails(props) {
     return (
-        <div>Detils</div>
+        <React.Fragment>
+            <h1>H1 Header</h1>
+            <h2>H2 Header</h2>
+            <h3>H2 Header</h3>
+            <p>Paragraph with <code>piece of code</code></p>
+            <p>Paragraph</p>
+            <ul>
+                <li>List item 1</li>
+                <li>List item 2</li>
+                <li>List item 3</li>
+            </ul>
+        </React.Fragment>
     )
 }
