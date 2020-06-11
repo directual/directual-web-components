97c93121249d95135b8f3338a4c95c4f769c7aee
Pavel Ershov <pavel@directual.com>
2020-06-11 13:11:30 +0300

    Inputs, Radio

--- example/src/App.js
+++ example/src/App.js
@@ -212,8 +212,54 @@ const App = (props) => {
               <ButtonsPage />
             </Route>
             <Route exact path="/system-data-entry">
-              <h1>Inputs, Selects. etc.</h1>
-              <Input />
+              <h1>Inputs, Selects, Checkboxes etc.</h1>
+              <Input 
+                label='Input (required)'
+                placeholder='Placeholder'
+                type='text'
+                required
+              />
+              <Input 
+                label='Input (not required + default value)'
+                placeholder='Placeholder'
+                defaultValue='default value'
+                type='text'
+              />
+              <Input 
+                label='Textarea (required)'
+                placeholder='Placeholder for textarea'
+                type='textarea'
+                rows={4}
+                required
+              />
+              <Input 
+                label='Password (required)'
+                placeholder='Type your password'
+                defaultValue=''
+                type='password'
+                required
+              />
+              <Input 
+                label='Radio station (default value)'
+                type='radio'
+                defaultValue='option2'
+                options={
+                  [
+                    {
+                        value: 'option1',
+                        label: 'Option 1'
+                    },
+                    {
+                        value: 'option2',
+                        label: 'Option 2'
+                    },
+                    {
+                        value: 'option3',
+                        label: 'Option 3'
+                    }
+                ]
+                }
+              />
 
             </Route>
 
--- src/components/fps/dataentry/input/input.js
+++ src/components/fps/dataentry/input/input.js
@@ -1,9 +1,96 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './input.module.css'
+import Radio from '../radio/radio'
 
 export default function Input(props) {
+    const [value, setValue] = useState(props.defaultValue)
+    const [pwdVisible, setPwdVisible] = useState('password')
+    const [warningMsg, setWarningMesg] = useState(props.warning || {})
+
+    const checkValue = () => {
+        (!value && props.required) ?
+            setWarningMesg({ type: 'error', msg: 'This field is required' }) :
+            setWarningMesg({});
+    }
+
+    const clearValue = () => {
+        setValue('')
+        props.required ?
+            setWarningMesg({ type: 'error', msg: 'This field is required' }) :
+            setWarningMesg({});
+    }
+
+    const handleChange = (e) => {
+        setValue(e.target.value)
+        props.required && setWarningMesg({});
+    }
+
+    // useEffect(
+    //     () => checkValue(), 
+    // [value])
 
     return (
-     <input type="text" value={props.value} placeholder={props.placeholder}/>   
+        <div className={styles.input_wrapper}>
+            <label>{props.label}</label>
+            {/* value: {value} */}
+
+            {props.type == 'text' &&
+                <div className={styles.field_wrapper}>
+                    <input
+                        className={`${styles.field} ${warningMsg.type && styles[warningMsg.type]}`}
+                        type="text"
+                        onChange={e => handleChange(e)}
+                        value={value}
+                        onBlur={checkValue}
+                        placeholder={props.placeholder}
+                    />
+                    {value &&
+                        <div className={`${styles.clear} icon icon-close`}
+                            onClick={clearValue}></div>}
+                </div>}
+
+            {props.type == 'textarea' &&
+                <div className={styles.field_wrapper}>
+                    <textarea
+                        className={`${styles.field} ${warningMsg.type && styles[warningMsg.type]}`}
+                        type="text"
+                        rows={props.rows || 3}
+                        onChange={e => handleChange(e)}
+                        value={value}
+                        onBlur={checkValue}
+                        placeholder={props.placeholder}
+                    />
+                    {value &&
+                        <div className={`${styles.clear} icon icon-close`}
+                            onClick={clearValue}></div>}
+                </div>}
+
+            {props.type == 'password' &&
+                <div className={styles.field_wrapper}>
+                    <input
+                        className={`${styles.field} ${warningMsg.type && styles[warningMsg.type]}`}
+                        type={pwdVisible}
+                        onChange={e => setValue(e.target.value)}
+                        value={value}
+                        onBlur={checkValue}
+                        placeholder={props.placeholder} />
+                    {pwdVisible == 'password' &&
+                        <div className={`${styles.clear} icon icon-view`}
+                            onClick={() => setPwdVisible('text')}></div>}
+                    {pwdVisible == 'text' &&
+                        <div className={`${styles.clear} icon icon-hide`}
+                            onClick={() => setPwdVisible('password')}></div>}
+                </div>}
+
+                {props.type == 'radio' &&
+                    <Radio
+                        onChange={e => setValue(e.target.value)}
+                        defaultValue={props.defaultValue}
+                        options={props.options}
+                    />
+                }
+            {warningMsg &&
+                <div className={`${styles.status} ${styles[warningMsg.type]}`}>{warningMsg.msg}</div>}
+        </div>
     )
 }
