7a0c760596bedd17d8f7a1338c2a0abdd5def49e
Pavel Ershov <pavel@directual.com>
2020-10-12 23:35:27 +0300

    FileUpload

--- src/components/fps/dataentry/fileupload/fileupload.js
+++ src/components/fps/dataentry/fileupload/fileupload.js
@@ -16,7 +16,7 @@ export default function FileUpload(props) {
                     <div {...getRootProps({ className: styles.dropzone })}>
                         <input {...getInputProps()} />
                         <p className={`icon icon-upload`}>
-                            {props.uploadText || "Drag'n'drop file here, or click to select one"}
+                            {props.uploadText || "Drop file here, or click to select one"}
                         </p>
                     </div>
                     <div className={styles.progress}>
@@ -35,7 +35,7 @@ export default function FileUpload(props) {
                     <br />
                     <div className={styles.dropzone}>
                         <p className={`icon icon-upload`}>
-                            Drag'n'drop files here, or click to select files
+                            Drop files here, or click to select files
                         </p>
                     </div>
                     <div className={styles.progress}>
@@ -45,25 +45,22 @@ export default function FileUpload(props) {
                         </div>
                         <div className={`icon icon-close ${styles.cancel}`}></div>
                     </div>
-                    <div className={styles.uploaded}>
-                        <div className={`icon icon-done ${styles.fileName}`}>
-                            <span>
-                                Uploaded: <strong>3</strong> files</span></div>
-                    </div>
+                    
                     <ul className={styles.multipleUpload}>
+                        <li><strong>Uploaded 3 files:</strong></li>
                         <li>
                             <span>
-                                <strong>Hello_world.jsx</strong>, 1.2 MB</span>
+                                Hello_world.jsx, 1.2 MB</span>
                             <div className={`icon icon-delete ${styles.delete}`}></div>
                         </li>
                         <li>
                             <span>
-                                <strong>Goodbye_Olympic_Bear.docx</strong>, 11.2 MB</span>
+                                Goodbye_Olympic_Bear.docx, 11.2 MB</span>
                             <div className={`icon icon-delete ${styles.delete}`}></div>
                         </li>
                         <li>
                             <span>
-                                <strong>War_and_peace.mov</strong>, 0.21 MB</span>
+                                War_and_peace.mov, 0.21 MB</span>
                             <div className={`icon icon-delete ${styles.delete}`}></div>
                         </li>
                     </ul>
