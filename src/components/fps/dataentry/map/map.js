041dfbb053f05a24ba2008527225a42e1906a754
Pavel Ershov <pavel@ershov.org>
2024-01-05 15:53:49 +0300

    test fps

--- src/components/fps/dataentry/map/map.js
+++ src/components/fps/dataentry/map/map.js
@@ -15,10 +15,10 @@ import { Map, Marker } from "pigeon-maps"
 export default function SimpleMap({ }) {
 
     return <div>
-        Итак,
-        <Map height={300} defaultCenter={[50.879, 4.6997]} defaultZoom={11}>
+        Итак, тут должна быть карта
+        {/* <Map height={300} defaultCenter={[50.879, 4.6997]} defaultZoom={11}>
             <Marker width={50} anchor={[50.879, 4.6997]} />
-        </Map>
+        </Map> */}
     </div>
 }
 
