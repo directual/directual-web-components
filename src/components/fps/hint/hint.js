8405fa1f4fd39f44a70e6a98f85c1f5906696bd1
Pavel Ershov <pavel@ershov.org>
2024-04-10 16:35:33 +0300

    FpsForm2

--- src/components/fps/hint/hint.js
+++ src/components/fps/hint/hint.js
@@ -9,10 +9,8 @@ import PropTypes from 'prop-types';
 
 export default function Hint(props) {
 
-
     const marginTop = (_.get(props,'margin.top') || _.get(props,'margin.top') === 0) ? _.get(props,'margin.top') : 24 
     const marginBottom = (_.get(props,'margin.bottom') || _.get(props,'margin.bottom') === 0) ? _.get(props,'margin.bottom') : 24 
-    console.log(props)
 
     return (
         <blockquote
