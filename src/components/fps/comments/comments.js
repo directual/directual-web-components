632cb103f750870bb6750a46fdceec46204c88ec
Pavel Ershov <pavel@ershov.org>
2024-06-24 00:55:50 +0300

    comments

--- src/components/fps/comments/comments.js
+++ src/components/fps/comments/comments.js
@@ -5,6 +5,7 @@ import Button from '../button/button'
 import Input from '../dataentry/input/input'
 import FileUpload from '../dataentry/fileupload/fileupload'
 import moment from 'moment'
+import { dict } from '../locale'
 
 import styles from './comments.module.css'
 import PropTypes from 'prop-types';
@@ -12,6 +13,14 @@ import icon from './../../../icons/fps-form2.svg'
 
 
 export default function Comments(props) {
+
+
+    const { auth, data, callEndpoint, onEvent, id, locale, handleRoute } = props
+    const lang = locale ? locale.length == 3 ? locale : 'ENG' : 'ENG'
+
+    console.log("=== comments data ===")
+    console.log(data)
+
 
     const commentsExample = [
         {
@@ -48,48 +57,96 @@ export default function Comments(props) {
         }
     ]
 
-    const [comments, setComments] = useState(commentsExample)
+    const [comments, setComments] = useState(_.get(data, "data") || [])
 
     return <div className={`${styles.comments} FPS_COMMENTS`}>
-        <CommentsHeader header="Comments" counter={12} {...props} />
+        <CommentsHeader header="Comments" counter={comments.length} {...props} />
         <AddComment {...props} />
         <div className={styles.commentsList}>
-            {comments.map(comment => <Comment comment={comment} {...props} key={comment.id} />)}
+            {comments
+                .filter(comment => !_.get(comment, _.get(data, "params._replyField")) || _.get(comment, _.get(data, "params._replyField")) == 'root')
+                .map(comment => <Comment {...props} comments={comments} comment={comment} key={comment.id} />)}
         </div>
     </div>
 }
 
 function Comment(props) {
-    const { comment } = props
+    const { comment, auth, data, comments, parent } = props
 
-    return <div className={`${styles.commentWrapper} FPS_COMMENT_WRAPPER`}>
-        <div className={styles.commentVote}>
+    const [addReply, setAddReply] = useState(false)
+
+    // форматируем дату для карточки (если есть) в соответствии с formatOptions
+    const formatDate = (value, formatOptions) => {
+        if (!value) { return null }
+        formatOptions = formatOptions || {}
+        const formattedDate = formatOptions.isUTC == 'true' ?
+            moment.utc(value).locale(formatOptions.dateLocale || 'ed-gb').format(formatOptions.dateFormat + formatOptions.timeFormat || 'DD/MM/Y, HH:mm, Z')
+            :
+            moment(value).locale(formatOptions.dateLocale || 'ed-gb').format(formatOptions.dateFormat + formatOptions.timeFormat || 'DD/MM/Y, HH:mm, Z')
+        return formattedDate
+    }
+
+    const commentID = _.get(comment, "id")
+    const commentText = _.get(comment, _.get(data, "params._textField"))
+    const commentDate = _.get(comment, _.get(data, "params._dateField"))
+    const formatCommentDate = _.get(_.find(_.get(data, "headers"), { sysName: _.get(data, "params._dateField") }), "formatOptions")
+    const commentAttachment = _.get(comment, _.get(data, "params._fileField")) ?
+        _.get(comment, _.get(data, "params._fileField")).split(",") : []
+    const commentAuthor = _.get(comment, _.get(data, "params._userField"))
+    const childComments = comments.filter(c => _.get(c, _.get(data, "params._replyField")) == commentID)
+    function shortenString(str, N) {
+        if (_.isString(str) && str.length > N) {
+            return _.truncate(str, { length: N + 3, omission: '...' });
+        }
+        return str;
+    }
+
+
+    return <div><div className={`${styles.commentWrapper} FPS_COMMENT_WRAPPER`}>
+        {/* <div className={styles.commentVote}>
             <div className={styles.commentVoteUp}>+</div>
             <div className={styles.commentVoteResult}>{comment.likes - comment.dislikes}</div>
             <div className={styles.commentVoteDown}>–</div>
-        </div>
+        </div> */}
         <div className={styles.commentBody}>
+            {parent && <div className={`${styles.commentParent} icon icon-arrowRight small`}>{parent}</div>}
             <div className={styles.commentBodyHeader}>
                 <div className={styles.commentBodyHeaderUserpic}>
-                    <img src={comment.author.userpic} />
+                    <img src={_.get(commentAuthor, "userpic")} />
                 </div>
                 <div className={styles.commentBodyHeaderName}>
-                    {comment.author.firstName} {comment.author.lastName}
+                    {_.get(commentAuthor, "firstName")} {_.get(commentAuthor, "lastName")}
                 </div>
                 <div className={styles.commentBodyHeaderDate}>
-                    {moment(comment.dateCreated).format("DD MMM YYYY")}
+                    {formatDate(commentDate, formatCommentDate)}
                 </div>
             </div>
             <div className={styles.commentBodyText}>
-                {comment.text}
+                {commentText}
             </div>
-            <div className={styles.commentBodyFiles}>
-                <a className={`icon icon-clip small ${styles.commentFileLink}`}>file name</a>
-            </div>
+            {commentAttachment && commentAttachment.length > 0 &&
+                commentAttachment.map(file => {
+                    const fileName = file.split("/")[file.split("/").length - 1]
+                    return <div className={styles.commentBodyFiles}>
+                        <a target="_blank" href={file}
+                            className={`icon icon-clip small ${styles.commentFileLink}`}>{fileName}</a>
+                    </div>
+                })}
             <div className={styles.commentBodyFooter}>
-                <div className={`icon icon-bubble small ${styles.commentReplyButton}`}>Reply</div>
+                <div onClick={e => setAddReply(true)} className={`icon icon-bubble small ${styles.commentReplyButton}`}>Reply</div>
             </div>
         </div>
+    </div>
+        {addReply && <div className={styles.childComments}>
+            <AddComment {...props} onCancel={() => setAddReply(false)} header="Reply" />
+        </div>}
+        {(childComments || []).length > 0 && <div className={`${styles.commentsList} ${styles.childComments}`}>
+            {(childComments || []).map(childComment => <Comment
+                {...props}
+                comment={childComment}
+                parent={`@${_.get(commentAuthor, "firstName")} ${_.get(commentAuthor, "lastName")}: ${shortenString(commentText, 64)}`}
+                key={childComment.id} />)}
+        </div>}
     </div>
 }
 
@@ -103,27 +160,47 @@ function CommentsHeader(props) {
 
 function AddComment(props) {
 
-    const { locale } = props
+    const { locale, onCancel, header, roles, assignTo } = props
+
+    const rolesExample = [
+        { key: 'admin', value: 'Admin' },
+        { key: 'security', value: 'Security officer' },
+        { key: 'finance', value: 'Finance department' },
+    ]
+    const usersExample = [
+        { key: 'pavel@directual.com', value: 'Pavel Ershov' },
+        { key: 'nikita@directual.com', value: 'Nikita Navalikhin' },
+    ]
 
     const [addFile, setAddFile] = useState(false)
+    const [showLock, setShowLock] = useState(!!roles)
+    const [showAssignTo, setShowAssignTo] = useState(!!roles)
     return <div className={styles.commentsAdd}>
-        <Input type='textarea' nomargin rows='auto' placeholder="Write text here..." />
+        <Input label={header} type='textarea' rows='auto' placeholder="Write text here..." />
+        <div className={styles.commentsAdditionslControls}>
+            {addFile && <FileUpload
+                locale={locale}
+                edit
+                allowUpload
+                host={'host'}
+                multiple={true}
+                onChange={value => console.log(value)}
+            />}
+            {showLock && <Input
+                label="User roles, who can see this comment"
+                options={rolesExample}
+                type='multiselect' icon='lock' placeholder="Choose roles..." />}
+            {showAssignTo && <Input
+                label="Assign to"
+                options={usersExample}
+                type='select' icon='checkbox' placeholder="Choose the user..." />}
+        </div>
         <div className={styles.commentsAddPanel}>
-            {addFile ?
-                <div className={styles.commentFileUpload}><FileUpload
-                    //images
-                    locale={locale}
-                    edit
-                    nomargin
-                    allowUpload
-                    host={'host'}
-                    multiple={true}
-                    onChange={value => console.log(value)}
-                />
-                    <div className={`${styles.commentActions}`} onClick={e => setAddFile(false)}>Cancel</div>
-                </div>
-                : <div className={`icon icon-clip ${styles.commentActions}`} onClick={e => setAddFile(true)} />}
+            <div className={`icon icon-clip ${styles.commentActions} ${addFile ? styles.active : ''}`} onClick={e => setAddFile(!addFile)} />
+            <div className={`icon icon-lock ${styles.commentActions} ${showLock ? styles.active : ''}`} onClick={e => setShowLock(!showLock)} />
+            <div className={`icon icon-checkbox ${styles.commentActions} ${showAssignTo ? styles.active : ''}`} onClick={e => setShowAssignTo(!showAssignTo)} />
             <Button accent icon="bubble">Send</Button>
+            {onCancel && <Button onClick={onCancel}>Cancel</Button>}
         </div>
     </div>
 }
@@ -148,8 +225,8 @@ Comments.settings = {
     isLegacy: false,
     isMarketplace: true,
     form: [
-      { name: 'Select API-endpoint', sysName: 'sl', type: 'api-endpoint' },
-      { name: 'Default HTTP request params', sysName: 'httpParams', type: 'httpParams' },
-      { name: 'Component comment', sysName: 'comment', type: 'comment' },
+        { name: 'Select API-endpoint', sysName: 'sl', type: 'api-endpoint' },
+        { name: 'Default HTTP request params', sysName: 'httpParams', type: 'httpParams' },
+        { name: 'Component comment', sysName: 'comment', type: 'comment' },
     ]
-  }
+}
