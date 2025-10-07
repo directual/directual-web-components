e034c459ec02b94a7b26eb11376830db002eca9c
dkotov899 <dkotov899@gmail.com>
2025-10-07 18:39:01 +0500

    up calendar

--- src/components/fps/bigCalendar/bigCalendar.js
+++ src/components/fps/bigCalendar/bigCalendar.js
@@ -4,207 +4,229 @@ import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
 import moment from 'moment';
 import _ from 'lodash';
 import PropTypes from 'prop-types';
-// import 'react-big-calendar/lib/css/react-big-calendar.css';
-// import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
-
-const CustomToolbar = (toolbar) => {
-  const goToBack = () => {
-    toolbar.onNavigate('PREV');
-  };
-
-  const goToNext = () => {
-    toolbar.onNavigate('NEXT');
-  };
-
-  const goToCurrent = () => {
-    toolbar.onNavigate('TODAY');
-  };
-
-  const label = () => {
-    return toolbar.label;
-  };
-
-  const onViewChange = (view) => {
-    toolbar.onView(view);
-  };
-
-  return (
-    <div className="rbc-toolbar">
-      <span className="rbc-btn-group">
-        <button type="button" onClick={goToBack}>
-          Back
-        </button>
-        <button type="button" onClick={goToCurrent}>
-          Today
-        </button>
-        <button type="button" onClick={goToNext}>
-          Next
-        </button>
-      </span>
-
-      <span className="rbc-toolbar-label">{label()}</span>
-
-      <span className="rbc-btn-group">
-        {toolbar.views.map((view) => (
-          <button
-            key={view}
-            type="button"
-            className={toolbar.view === view ? 'rbc-active' : ''}
-            onClick={() => onViewChange(view)}
-          >
-            {view}
-          </button>
-        ))}
-      </span>
-
-      <span className="rbc-btn-group">
-        <button
-          type="button"
-          className="calendar__save-button"
-          onClick={() => alert('Save!')}
-        >
-          Save
-        </button>
-      </span>
-    </div>
-  );
-};
+import styles from './bigCalendar.module.css';
 
 const localizer = momentLocalizer(moment);
 const DnDCalendar = withDragAndDrop(Calendar);
 
-const Events = [
-  {
-    id: 1,
-    start: moment().toDate(),
-    end: moment().add(1, 'h').toDate(),
-    title: 'Event-1',
-    mode: 'event'
-  },
-  {
-    id: 2,
-    start: moment().subtract(3, 'h').toDate(),
-    end: moment().subtract(2, 'h').toDate(),
-    title: 'Event-2',
-    mode: 'event'
-  },
-  {
-    id: 3,
-    title: 'Background-1',
-    start: moment().add(2, 'days').toDate(),
-    end: moment().add(2, 'days').toDate(),
-    mode: 'background'
-  },
-  {
-    id: 4,
-    title: 'Background-2',
-    start: moment().add(3, 'days').toDate(),
-    end: moment().add(5, 'days').toDate(),
-    mode: 'background'
-  }
-];
+export default function BigCalendar(props) {
+    const { data, auth, locale, callEndpoint } = props;
+    const [state, setState] = useState({
+        events: [],
+        backEvents: []
+    });
 
-export default function BigCalendar({ data }) {
-  const [events, setEvents] = useState(Events);
-  const [backEvents, setBackEvents] = useState(_.get(data, 'backEvents', null));
-
-  useEffect(() => {
-    setEvents(_.filter(Events, (event) => event.mode === 'event'));
-    setBackEvents(_.filter(Events, (event) => event.mode === 'background'));
-  }, []);
-
-  const onEventResize = (data) => {
-    const { event, start, end } = data;
-    setEvents(
-      events.map((e) => {
-        if (e.id === event.id) {
-          e.start = start;
-          e.end = end;
+    // Нормализация дат - приводим любой формат к Date объекту
+    const normalizeDate = (date) => {
+        if (!date) return null;
+        
+        // Если уже Date объект - возвращаем как есть
+        if (date instanceof Date) return date;
+        
+        // Пробуем парсить через moment (умеет ISO, timestamp, строки)
+        const momentDate = moment(date);
+        if (momentDate.isValid()) {
+            return momentDate.toDate();
         }
-        return e;
-      })
-    )
-  };
+        
+        // Если всё сломалось - логируем и возвращаем текущую дату
+        console.warn('Invalid date format:', date, '- using current date');
+        return new Date();
+    };
 
-  const onEventDrop = (data) => {
-    const { event, start, end } = data
-    setEvents(
-      events.map((e) => {
-        if (e.id === event.id) {
-          e.start = start;
-          e.end = end;
+    // Нормализация события - обрабатываем start и end
+    const normalizeEvent = (event) => {
+        return {
+            ...event,
+            start: normalizeDate(event.start),
+            end: normalizeDate(event.end)
+        };
+    };
+
+    useEffect(() => {
+        const rawEvents = _.get(data, "__data__.sl_events", []);
+        setState({
+            events: _.filter(rawEvents, (event) => event.mode === 'event').map(normalizeEvent),
+            backEvents: _.filter(rawEvents, (event) => event.mode === 'background').map(normalizeEvent)
+        });
+    }, [data]);
+
+    const onEventResize = (data) => {
+        const { event, start, end } = data;
+        setState(prevState => ({
+            ...prevState,
+            events: prevState.events.map((e) => 
+                e.id === event.id 
+                    ? { ...e, start, end }
+                    : e
+            )
+        }));
+    };
+
+    const onEventDrop = (data) => {
+        const { event, start, end } = data;
+        setState(prevState => ({
+            ...prevState,
+            events: prevState.events.map((e) => 
+                e.id === event.id 
+                    ? { ...e, start, end }
+                    : e
+            )
+        }));
+    };
+
+    const handleSelectEvent = useCallback((event) => 
+        window.alert(event.title),
+        []
+    );
+
+    const handleSelectSlot = useCallback(({ start, end }) => {
+        const title = window.prompt('New Event name');
+        if (title) {
+            setState(prevState => ({
+                ...prevState,
+                events: [...prevState.events, { start, end, title }]
+            }));
         }
-        return e;
-      })
-    )
-  };
+    }, []);
 
-  const handleSelectEvent = useCallback(
-    (event) => window.alert(event.title),
-    []
-  );
+    const { defaultDate, scrollToTime } = useMemo(() => ({
+        defaultDate: moment().toDate(),
+        scrollToTime: new Date(1970, 1, 1, 6)
+    }), []);
 
-  const handleSelectSlot = useCallback(
-    ({ start, end }) => {
-      const title = window.prompt('New Event name')
-      if (title) {
-        setEvents((prev) => [...prev, { start, end, title }]);
-      }
-    },
-    [setEvents]
-  );
+    // Конвертация Date объектов в ISO строки для отправки в базу
+    const serializeEvent = (event) => {
+        return {
+            ...event,
+            start: event.start instanceof Date ? event.start.toISOString() : event.start,
+            end: event.end instanceof Date ? event.end.toISOString() : event.end
+        };
+    };
 
-  const { defaultDate, scrollToTime } = useMemo(
-    () => ({
-      defaultDate: moment().toDate(),
-      scrollToTime: new Date(1970, 1, 1, 6)
-    }),
-    []
-  );
+    const handleSave = () => {
+        callEndpoint && callEndpoint(
+            _.get(data, "params.sl_actions"),
+            "POST",
+            { 
+                events: state.events.map(serializeEvent), 
+                backEvents: state.backEvents.map(serializeEvent) 
+            },
+            undefined,
+            (result, data) => {
+                console.log('Saved events:', result);
+                console.log('Response:', data);
+            }
+        );
+    };
 
-  return (
-    <div>
-      <DnDCalendar
-        components={{
-          toolbar: CustomToolbar
-        }}
-        defaultDate={defaultDate}
-        defaultView='month'
-        events={events}
-        backgroundEvents={backEvents}
-        localizer={localizer}
-        style={{ height: '100vh' }}
-        onEventDrop={onEventDrop}
-        onEventResize={onEventResize}
-        resizable
-        popup
-        onSelectEvent={handleSelectEvent}
-        onSelectSlot={handleSelectSlot}
-        selectable
-        scrollToTime={scrollToTime}
-        //culture={data.locale ? data.locale : "en"}
-        culture="ru-RU"
-      />
-    </div>
-  );
+    const CustomToolbar = (toolbar) => {
+        const goToBack = () => {
+            toolbar.onNavigate('PREV');
+        };
+    
+        const goToNext = () => {
+            toolbar.onNavigate('NEXT');
+        };
+    
+        const goToCurrent = () => {
+            toolbar.onNavigate('TODAY');
+        };
+    
+        const label = () => {
+            return toolbar.label;
+        };
+    
+        const onViewChange = (view) => {
+            toolbar.onView(view);
+        };
+    
+        return (
+            <div className="rbc-toolbar">
+                <span className="rbc-btn-group">
+                    <button type="button" onClick={goToBack}>
+                        Back
+                    </button>
+                    <button type="button" onClick={goToCurrent}>
+                        Today
+                    </button>
+                    <button type="button" onClick={goToNext}>
+                        Next
+                    </button>
+                </span>
+    
+                <span className="rbc-toolbar-label">{label()}</span>
+    
+                <span className="rbc-btn-group">
+                    {toolbar.views.map((view) => (
+                        <button
+                            key={view}
+                            type="button"
+                            className={toolbar.view === view ? 'rbc-active' : ''}
+                            onClick={() => onViewChange(view)}
+                        >
+                            {view}
+                        </button>
+                    ))}
+                </span>
+    
+                <span className="rbc-btn-group">
+                    <button
+                        type="button"
+                        className="calendar__save-button"
+                        onClick={handleSave}
+                    >
+                    Save
+                    </button>
+                </span>
+            </div>
+        );
+    };
+
+    return (
+        <div>
+            <DnDCalendar
+                className={`${styles.rbc_calendar} D_FPS_BIG_CALENDAR`}
+                components={{
+                    toolbar: CustomToolbar
+                }}
+                defaultDate={defaultDate}
+                defaultView='month'
+                events={state.events}
+                backgroundEvents={state.backEvents}
+                localizer={localizer}
+                onEventDrop={onEventDrop}
+                onEventResize={onEventResize}
+                resizable
+                popup
+                onSelectEvent={handleSelectEvent}
+                onSelectSlot={handleSelectSlot}
+                selectable
+                scrollToTime={scrollToTime}
+                //culture={data.locale ? data.locale : "en"}
+                culture="ru-RU"
+                style={{ height: '100vh' }}
+            />
+        </div>
+    );
 }
 
 BigCalendar.propTypes = {
-  data: PropTypes.object,
-  auth: {},
-  locale: {}
+    data: PropTypes.object,
+    auth: {},
+    locale: {}
 }
   
 BigCalendar.defaultProps = {
-  data: {},
-  auth: {},
-  locale: 'ENG'
+    data: {},
+    auth: {},
+    locale: 'ENG'
 }
   
 BigCalendar.settings = {
-  //icon: icon,
-  name: 'Calendar',
-  sysName: 'BigCalendar',
-  isLegacy: false,
-  isMarketplace: true
+    //icon: icon,
+    name: 'Calendar',
+    sysName: 'BigCalendar',
+    isLegacy: false,
+    isMarketplace: true
 }
--- src/stories/BigCalendar.stories.js
+++ src/stories/BigCalendar.stories.js
@@ -17,13 +17,33 @@ export default {
 
 const CalendarData = 
 {
-    "locale": "",
-    "libs": [],
-    "header": "",
-    "initialize": "",
-    "update": "",
-    "destroy": "",
-    "data": {}
+    "sl_events": "events",
+    "__libs__": [],
+    "__header__": "",
+    "__initialize__": null,
+    "__update__": null,
+    "__destroy__": null,
+    "__data__": {
+        "sl_events": [
+            {
+                "id": "0b2a2600-0b6b-4bf2-888a-086912392845",
+                "mode": "event",
+                "end": 1759950000000,
+                "title": "Event-1",
+                "start": 1759863600000
+            },
+            {
+                "id": "662a453b-f4cc-42ce-b1ad-264ea1bf0326",
+                "mode": "event",
+                "end": 1759888800000,
+                "title": "Event-2",
+                "start": 1759863600000
+            }
+        ],
+        "_api_": {
+            "sl_events": "https://api.directual.com/good/api/v5/data/events/events?appID=0d165fd9-a082-4cdf-8e9a-ea69f794698c"
+        }
+    }
 }
 
 export const Regular = {
