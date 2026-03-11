bb6a1e32396c1dfe06d39450e9ba8f35e67688fb
paveler <pavel@directual.com>
2026-03-11 17:33:53 +0300

    html comp_ID

--- src/components/fps/FpsHtml.js
+++ src/components/fps/FpsHtml.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 //import icon from './../../icons/fps-rich-text.svg'
 import iconHtml from './../../icons/fps-html.svg'
 import iconMkd from './../../icons/fps-markdown.svg'
@@ -6,19 +6,30 @@ import { ComponentWrapper } from './wrapper/wrapper'
 import Article from './article/article'
 import Input from './dataentry/input/input'
 import InnerHTML from 'dangerously-set-html-content'
+import _ from 'lodash'
 
 export default function FpsHtml(props) {
 
-  const { socket } = props
+  const { socket, id, callEndpoint } = props
 
   const [data, setData] = useState(props.data)
   const [renderKey, setRenderKey] = useState(0)
-  
+  const [hidden, setHidden] = useState(false)
+
+  const wrapperRef = useRef(null)
+  const dataRef = useRef(data)
+  const htmlRef = useRef((props.data || {}).html || '')
+
   useEffect(() => {
     if (JSON.stringify(props.data) !== JSON.stringify(data)) {
       setData(props.data)
     }
   }, [props])
+
+  useEffect(() => {
+    dataRef.current = data
+    htmlRef.current = (data || {}).html || ''
+  }, [data])
 
   // При изменении socket (т.е. при обновлениях через веб-сокет)
   // форсим ремаунт чтобы скрипты отработали заново
@@ -26,18 +37,53 @@ export default function FpsHtml(props) {
     setRenderKey(prev => prev + 1)
   }, [socket])
 
+  // Регистрируем API в window для доступа из внешнего кода
+  useEffect(() => {
+    const componentId = _.get(data, 'params.comp_ID') || id
+    if (!componentId) return
+
+    if (!window.FpsHtml_API) {
+      window.FpsHtml_API = {}
+    }
+
+    window.FpsHtml_API[componentId] = {
+      getHtml: () => htmlRef.current,
+      setHtml: (newHtml) => {
+        setData(prev => ({ ...(prev || {}), html: newHtml }))
+      },
+      rerender: () => setRenderKey(prev => prev + 1),
+      show: () => setHidden(false),
+      hide: () => setHidden(true),
+      getData: () => dataRef.current,
+      getElement: () => wrapperRef.current,
+      callEndpoint: (endpoint, method, body, params, finish) => {
+        if (callEndpoint) {
+          callEndpoint(endpoint, method, body, params, finish)
+        }
+      },
+    }
+
+    return () => {
+      if (window.FpsHtml_API && window.FpsHtml_API[componentId]) {
+        delete window.FpsHtml_API[componentId]
+      }
+    }
+  }, [data, id, callEndpoint])
+
   const html = (data || {}).html || ''
-  if (data && data.isHidden == 'true') { return <div /> }
+  if (hidden || (data && data.isHidden == 'true')) { return <div /> }
 
   // console.log('=== FpsHTML ===')
   // console.log(data)
 
   return (
-    <ComponentWrapper>
-      <Article>
-        {html && <InnerHTML key={renderKey} allowRerender={true} html={html} />}
-      </Article>
-    </ComponentWrapper>
+    <div ref={wrapperRef}>
+      <ComponentWrapper>
+        <Article>
+          {html && <InnerHTML key={renderKey} allowRerender={true} html={html} />}
+        </Article>
+      </ComponentWrapper>
+    </div>
   )
 }
 
@@ -46,6 +92,7 @@ FpsHtml.settings = {
   name: "HTML code",
   sysName: 'FpsHtml',
   form: [
+    { name: 'Comp ID', sysName: 'comp_ID', type: 'string' },
     { name: "Enter your HTML-code", sysName: "html", type: "html-SLenriched" },
     { name: 'Apply template engine', sysName: 'withTemplate', type: 'boolean' },
     { name: 'API-endpoint (optional)', sysName: 'sl', type: 'api-endpoint' },
@@ -71,6 +118,7 @@ FpsMarkdown.settings = {
   name: "Markdown text",
   sysName: 'FpsMarkdown',
   form: [
+    { name: 'Comp ID', sysName: 'comp_ID', type: 'string' },
     { name: "Enter your text", sysName: "markdown", type: "markdown-SLenriched" },
     { name: 'Apply template engine', sysName: 'withTemplate', type: 'boolean' },
     { name: 'API-endpoint (optional)', sysName: 'sl', type: 'api-endpoint' },
